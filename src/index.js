e9b630885afb8dada266a4cb02244e4aa8ff2dca
Nikita213-hub <70839622+Nikita213-hub@users.noreply.github.com>
2021-07-07 22:20:47 +0300

    fix: fixed negative nums

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 module.exports = function reverse (n) {
   let array = n.toString().split("");
   let ArrReversed = array.reverse().join("");
+  if (n < 0){
+      ArrReversed = ArrReversed * (-1);
+  }
   return ArrReversed
 }
